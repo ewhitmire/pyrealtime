46e07b2032a183a756fb05613acb645923cd082a
Eric Whitmire <emwhit@cs.washington.edu>
2017-07-29 14:25:59 -0700

    uploading to pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
 setup(
     name="PyRealtime",
-    version="0.1",
+    version="0.1.1",
     packages=['pyrealtime'],
+    description='A package for realtime data processing, including reading from serial ports and plotting.',
+    author='Eric Whitmire',
+    author_email='emwhit@cs.washington.edu',
+    url='https://github.com/ewhitmire/pyrealtime',
+    keywords=['realtime', 'plotting', 'serialport', 'logging', 'pipeline']
 )
