46b3572b3cb84f0cfa9a8400457d89966b906518
Eric M Whitmire <emwhit@cs.washington.edu>
2017-11-02 10:09:32 -0700

    version 0.1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="PyRealtime",
-    version="0.1.4",
+    version="0.1.5",
     packages=['pyrealtime'],
     description='A package for realtime data processing, including reading from serial ports and plotting.',
     author='Eric Whitmire',
