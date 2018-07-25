e7b3fcba825fdc76c8c42d5ddfbb923131529a4c
Eric Whitmire <ewhitmire91@gmail.com>
2018-07-25 00:16:31 -0700

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,17 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ['matplotlib', 'matplotlib.animation', 'matplotlib.widgets']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 from pyrealtime import __version__
 
 # -- General configuration ------------------------------------------------
